73f7b12acad7a4d5817444724e2ba72ef59a1b0a
Nieliton Rodrigues <nieliton.sousa3040@gmail.com>
2022-02-09 18:19:43 -0300

    fix: round total

--- src/components/CartTotal/index.jsx
+++ src/components/CartTotal/index.jsx
@@ -7,8 +7,10 @@ function CartTotal({currentSale}){
             <p>Total</p>
             <span>
                 R$ {currentSale.reduce((acc, cur) => {
-                    return acc+cur.price
-                }, 0)}
+                    let total = acc+cur.price
+                    let newTotal = Math.round(total, -1)
+                    return newTotal
+                }, 0)},00
             </span>
         </div>
     )
